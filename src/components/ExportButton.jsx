df6a4851303d3f218aa263dd5e9af2655980fdc0
Walid <walid.seoudi@gmail.com>
2019-06-25 07:50:30 +0200

    add link to download fr and en files

--- src/components/ExportButton.jsx
+++ src/components/ExportButton.jsx
@@ -1,15 +1,44 @@
 import React from 'react';
 import { jsonFromTranslations } from '../utils/exportToJson'
 
-const ExportButton = ({translations}) => {
-  const exportToJson = (event) => {
-    event.preventDefault()
-    let jsons = jsonFromTranslations(translations)
-    console.log(jsons)
-    // TODO iterate throws 'jsons' arrays and add link to download each or zipot !
+class ExportButton extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { jsons: [] }
   }
 
-  return (<button onClick={e => exportToJson(e)}>Export</button>)
+  exportToJson(event) {
+    event.preventDefault()
+    let jsons = jsonFromTranslations(this.props.translations)
+    this.setState({jsons})
+  }
+
+  render () {
+    return (
+      <div>
+        <button onClick={e => this.exportToJson(e)}>Export</button>
+        { this.renderLinks() }
+      </div>
+    )
+  }
+
+  renderLinks() {
+    return this.state.jsons.map(json => {
+      let filename = `${Object.keys(json)[0]}.json`
+      return (
+        [<br/>,
+          <a href={hrefFromJson(json)} key={filename}
+             download={filename}>
+            {filename}
+          </a>
+        ]
+      )
+    })
+  }
 }
 
 export default ExportButton
+
+function hrefFromJson(json) {
+  return `data:application/json;charset=utf-8,${encodeURIComponent(JSON.stringify(json))}`
+}
